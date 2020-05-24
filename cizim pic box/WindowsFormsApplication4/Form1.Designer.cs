773613a6e2d512edbff604df37b252096d3e7625
sadık yıldırım <sadik_yildirim@outlook.com>
2020-05-24 20:02:59 +0300

    v1
    v1

---
+++ cizim pic box/WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication4.Properties.Resources.Ekran_Aldsadıntısı;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(442, 429);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 508);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(532, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 548);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ cizim pic box/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = pictureBox1.PointToClient(Cursor.Position).ToString();
+            if(cizim=true)
+            {
+
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+       bool cizim = false;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            cizim = true;
+
+            label1.Text = pictureBox1.PointToClient(Cursor.Position).ToString();
+            //mouse un picbx uzerindeki konumunu alır
+            //picturebox yerine this button gibi şeylerin uzerindeki konumunuda alabiliriz.
+            //lazım olabilir
+            string[] dizi = { "{", "}", "X", "Y", "=" };
+
+            char[] dizi1 = label1.Text.ToCharArray();
+            string y = "", x = "";
+            for (int i = 0; i < dizi1.Length; i++)
+            {
+                int a = 0;
+                for (int j = 0; j < dizi.Length; j++)
+                {
+                    if (dizi1[i].ToString()== dizi[j])
+                    {
+                        continue;                  
+                    }
+                    a++;
+                  
+                }
+                if (a == 5)
+                {
+                    if (dizi1[i].ToString() == ",")
+                    {
+                        x = y;
+                        y = "";
+                        continue;
+                    }
+                    y += dizi1[i];
+                }
+
+            }
+         /*   MessageBox.Show(x); MessageBox.Show(y);*/
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            bmp.SetPixel(int.Parse(x), int.Parse(y), Color.Black); //seçilen pixel
+            bmp.SetPixel(int.Parse(x), int.Parse(y)-1, Color.Black);//sol tarafındaki pixel
+            bmp.SetPixel(int.Parse(x), int.Parse(y)+1, Color.Black);//sağ tarafındaki   
+            bmp.SetPixel(int.Parse(x)+1, int.Parse(y), Color.Black);//altındaki
+            bmp.SetPixel(int.Parse(x)-1, int.Parse(y), Color.Black);//üstündeki
+            bmp.SetPixel(int.Parse(x)-1, int.Parse(y)+1, Color.Black);//sağ üst çaprazı
+            bmp.SetPixel(int.Parse(x)+1, int.Parse(y)-1, Color.Black);//sol alt çaprazı
+            bmp.SetPixel(int.Parse(x)+1, int.Parse(y)+1, Color.Black);//sağ alt çaprazı
+            bmp.SetPixel(int.Parse(x)-1, int.Parse(y)-1, Color.Black);//sol üst çaprazı
+            // bunları daha net gorebilmek için büyüttüm 
+            //fırca boyutları yapıcam orda bu sekil işime yarıyacak (dongu ile yap)
+            pictureBox1.Image = bmp;
+
+
+
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            cizim = false;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save(@"C:\Users\sadik\Desktop\"+"asdasd.png");
+        }
+    }
+}
